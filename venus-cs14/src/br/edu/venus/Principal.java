9dd367918ba52b07db900df44c8fbda3ec8a16af
Luis Roth <vitlroth@gmail.com>
2019-07-29 10:34:55 -0300

    * Criada classe principal e comentários.

---
+++ venus-cs14/src/br/edu/venus/Principal.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package br.edu.venus;
+
+/**
+ * @author vitlr
+ *
+ */
+public class Principal {
+
+	/**
+	 * Um estudo sobre estrutura de dados
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Método principal
+
+	}
+
+}
